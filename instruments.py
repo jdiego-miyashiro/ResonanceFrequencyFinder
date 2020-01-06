15d161b4e032b2c5f65e0b8f0bbecb2e35794dce
jdiego-miyashiro <castromi@ualberta.ca>
2020-01-05 22:14:56 -0700

    Update instruments.py

--- instruments.py
+++ instruments.py
@@ -6,21 +6,22 @@ Created on Fri Jan  3 09:09:18 2020
 """
 
 import math
+import random as rnd
 
-class function_generator:
+class function_generator_simulator:
     def __init__(self,frequency):
         self.current_frequency = frequency
-        self.read_termination ='\n'                #for compatibility reasons
-        self.write_termination='\n'                #for compatibility reasons 
+        self.read_termination ='\n'                #this is here just for compatibility reasons
+        self.write_termination='\n'                #this is heree just for compatibility reasons 
         
     def write(self,frequency_string):
-        self.current_frequency=frequency_string.split("Q")[1]  #Split the input string and take the number
-
-
-class lock_in:
-    def __init__(self,function_generator):  #it will pass the function_generator instance to here
-        self.current_frequency=float(function_generator.current_frequency)
-        
+        self.current_frequency=float(frequency_string.split("Q")[1])  #Split the input string and take the number
+       
     def query(self,dummy_string):              #the string is just for compatibility
-        phase = (100)*math.sin(0.005*self.current_frequency) + (4E-6)*(self.current_frequency)^2
+        
+        phase = (100)*math.sin(0.005*self.current_frequency) + ((4E-6)*(self.current_frequency)**2) + rnd.uniform(0,30)
+        
         return phase
+  
+    
+
